c878eb81b17085d70d3fc83510beed3c07882efd
8it.sequence <8it.sequence@gmail.com>
2020-01-24 23:53:53 +0100

    Add spring boot dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,26 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
+    id("org.springframework.boot") version "2.2.2.RELEASE"
+    id("io.spring.dependency-management") version "1.0.8.RELEASE"
     kotlin("jvm") version "1.3.61"
+    kotlin("plugin.spring") version "1.3.61"
 }
 
 group = "com.8itsequence.apps.spring"
 version = "0.0.1"
+
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+val developmentOnly: Configuration by configurations.creating
+configurations {
+    runtimeClasspath {
+        extendsFrom(developmentOnly)
+    }
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
 
 repositories {
     mavenCentral()
@@ -11,13 +28,32 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("reflect"))
+
+    implementation(springBootStarter("web"))
+
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+    }
 }
 
 tasks {
-    compileKotlin {
-        kotlinOptions.jvmTarget = "1.8"
+    withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = "1.8"
+        }
     }
-    compileTestKotlin {
-        kotlinOptions.jvmTarget = "1.8"
+
+    withType<Test> {
+        useJUnitPlatform()
     }
 }
+
+fun DependencyHandler.springBootStarter(module: String, version: String? = null): Any =
+    "org.springframework.boot:spring-boot-starter-$module${version?.let { ":$version" } ?: ""}"
